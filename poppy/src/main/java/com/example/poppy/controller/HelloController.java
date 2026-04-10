fa902ac62ac5c87650362ece7fed461b2f422dd5
Gopireddy-123 <161678256+Gopireddy-123@users.noreply.github.com>
2026-04-10 13:00:12 +0530

    update HelloController

---
+++ poppy/src/main/java/com/example/poppy/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.poppy.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+
+}
